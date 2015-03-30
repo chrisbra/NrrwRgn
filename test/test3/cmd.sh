997308c0dcadfb1081abd7fcc1cd2e82e299f80e
Christian Brabandt <cb@256bit.org>
2015-03-30 22:06:27 +0200

    added test3

---
+++ test/test3/cmd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#set -x
+
+Test="Test3"
+
+dir="$(realpath ../..)"
+LC_ALL=C vim -u NONE -N \
+    --cmd ':set noswapfile hidden' \
+    -c "sil :so $dir/plugin/NrrwRgn.vim" \
+    -c 'sp description.md | noa wincmd p | :e 1.txt' \
+    -c 'sil :1,$NR' \
+    -c 'sil :$put =\"Added Line\"' \
+    -c 'sil :wq' \
+    -c 'sil :$put =\"Added after Narrowing Line\"' \
+    -c ':bufdo if bufname("")=~"^\\d\\.txt$"|saveas! %.mod|endif' \
+    -c ':qa'
+
+rt=$(diff -uN0 <(cat *.mod) <(cat *.ok))
+if [ "$?" -ne 0 ]; then
+    printf "$Test failed\n"
+    printf "Diff:\n%s" "$rt"
+    exit 2;
+else
+    printf "$Test successful!\n"
+fi
---
+++ test/test3/description.md
@@ -0,0 +1,1 @@
+## Test 2: Single Narrowed Window for a single buffer, but has another split window ##
